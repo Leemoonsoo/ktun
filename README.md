9d0de92c40b200ad3665da6d7c04aa23fcfbf8b7
Lee moon soo <leemoonsoo@gmail.com>
2018-10-03 04:56:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# kude-tun
+
+kude-tun establishes http reverse tunnels over WebScoket connections for circumventing the problem of directly connect to hosts behind a strict firewall or without public IP
+
+- Reverse http tunnel over websocket
+- Expose service running behind firewall with access token
+- Http request 
+
+## Installation
+
+```
+npm install @zarvis/kude-tun
+```
+
+## Usage
+
+
+### Setup tunnel server
+Start a tunnel server (on port 8080), on publicly accessible host.
+
+```
+kude-tun -p 8080
+```
+
+### Start tunnel
+If public domain name of tunnel server is `ktun.example.com`, kude-tun client can connect the server to reverse proxy service behind firewall.
+
+```
+kude-tun -t mysecrettoken -r localhost:7777 -s ws://ktun.example.com/_ktuncreate
+```
+
+kude-tun client request a new tunnel to server (with secret token 'mysecrettoken'), and reverse proxy request from server to service behind firewall `localhost:7777`.
+
+### Make a request through the tunnel
+
+```
+$ curl 'http://ktun.example.com:8080/_ktun/my_b!g_$secret/some/web/page'
+$ curl '-HX-Ktun-Token: mysecrettoken' http://wstun.example.com:8080/some/web/page
+```
+
+http request to `http://wstun.example.com/_ktun/mysecrettoken/` will be forwarded to service running behind firewall (`localhost:7777`) through the tunnel.
+
+```
+      HTTP-client ==>\     ||firewall||     /===> HTTP-server
+                     |                      | (localhost:7777)
+                     \----------------------/
+                Kude-tun  <===tunnel==== Kude-tun
+          (tunnel-server.com)            (client)
+```
+
+This connection mechanism is inspired by [wstunnel](https://github.com/rightscale/wstunnel). However, [wstunnel](https://github.com/rightscale/wstunnel) does not handle websocket request. This project uses [wstun](https://github.com/MDSLab/wstun) to create tcp tunnel over websocket and [node-http-proxy](https://github.com/nodejitsu/node-http-proxy) to reverse proxy request. And it handles websocket connection smoothly.
---
+++ index.js
@@ -0,0 +1,152 @@
+log4js = require('log4js');
+var http = require('http'),
+    httpProxy = require('http-proxy'),
+    wstun = require('@mdslab/wstun'),
+    getPort = require('get-port');
+    
+log4js.configure({
+  appenders: {
+    console: { type: 'console' }
+  },
+  categories: {
+    ktun: { appenders: ['console'], level: 'info' },
+    wstun: { appenders: ['console'], level: 'error' },
+    default: { appenders: ['console'], level: 'info' }
+  }
+});
+
+const logger = log4js.getLogger('ktun');
+
+var program = require('commander');
+
+program
+  .version('0.1.0')
+  .option('-p, --port [number]', '(server mode) Tunnel server port to listen')
+  .option('-s, --server [url]', '(client mode) Tunnel server address to connect. e.g. ws://proxyserverhost:8080/_ktuncreate')
+  .option('-t, --tunnel [name]', '(client mode) Tunnel name to create. [a-z0-9]*. Tunnel name should be unique')
+  .option('-r, --proxy [host:port]', '(client mode) Proxy target host:port. e.g. (localhost:8888)')
+  .parse(process.argv);
+
+if (program.port) {
+  getPort().then(function(tport) {
+    logger.info("Start server on ", program.port)
+    createServer({
+      proxyPort: program.port,
+      tunnelServerPort: tport
+    })
+  })
+} else if (program.server && program.tunnel && program.proxy) {
+  const client = new wstun.client_reverse;
+  client.start(program.tunnel, program.server, program.proxy);
+  logger.info("Starting tunnel", program.proxy)
+} else {
+  program.help()
+}
+
+function createServer(config) {
+  function resolveTokenFromRequest(req) {
+      var token = undefined
+      if (req.url && req.url.startsWith("/_ktun/")) {
+          var m = /[/]_ktun[/]([^/]+)(.*)/.exec(req.url)
+          token = m[1]
+          req.url = m[2]
+      } else if (req.headers && req.headers['X-Ktun-Token']) {
+          token = req.headers['X-Ktun-Token']
+      } else if (req.host && req.host.startsWith("tun-")) {
+          var m = /tun-([^-.]+).*/.exec(req.host)
+          token = m[1]
+      }
+      return token    
+  }
+
+  var proxy = httpProxy.createProxyServer({ws: true})
+
+  /**
+   * Key is token
+   * Value is
+   * {
+   *   port: <port number>,
+   *   socket: <websocket object>
+   * }
+   */
+  var tunnelMap = {}
+  var count = 0
+  var proxyServer = http.createServer(function(req, res) {
+    // You can define here your custom logic to handle the request
+    // and then proxy the request.
+    const token = resolveTokenFromRequest(req)
+    if (token && tunnelMap[token]) {
+      logger.info("Proxy request to tunnel", token, req.url)
+      proxy.web(req, res, { target: 'http://localhost:' + tunnelMap[token].port });
+    } else {
+      // error
+      logger.warn("Can't proxy request. tunnel not found", token, req.url)
+    }
+  });
+  //
+  // Listen to the `upgrade` event and proxy the
+  // WebSocket requests as well.
+  //
+  proxyServer.on('upgrade', function (req, socket, head) {
+    if (req.url.startsWith("/_ktuncreate/?dst=")) {
+      var m = /([^?]+[?]dst=)([^:]+)[:](.*)/.exec(req.url)
+      var token = m[3]
+      if (tunnelMap[token]) {
+        // token already exists
+        // error
+        return
+      }
+      getPort().then(function(port) {
+        if (tunnelMap[token] || Object.keys(tunnelMap).find((k) => tunnelMap[k].port == port)) {
+          // token or port already in the map
+          // error
+          return
+        } else {
+          tunnelMap[token] = {
+            port: port,
+            socket: socket
+          }
+          socket._token = token
+          // rewrite url
+          url = m[1] + "localhost:" + port
+          logger.info("Tunnel open", token, port)
+          req.url = url
+          proxy.ws(req, socket, head, { target: 'http://localhost:' + config.tunnelServerPort });
+        }
+      })
+    } else if (req.url.startsWith("/_ktuncreate/?id=")) {
+      logger.trace("websocket connection", req.url)
+      proxy.ws(req, socket, head, { target: 'http://localhost:' + config.tunnelServerPort });
+    } else { // proxy traffic
+      var token = resolveTokenFromRequest(req)
+      if (token && tunnelMap[token]) {
+        logger.info("Proxy ws request to tunnel", token, req.url)
+        proxy.ws(req, socket, head, { target: 'http://localhost:' + tunnelMap[token].port });
+      } else {
+        logger.warn("Can't proxy websocket request. tunnel not found", token, req.url)
+      }
+    }
+  });
+
+  proxy.on('open', function (socket) {
+    logger.trace('connection open');
+  });
+
+  proxy.on('close', function (res, socket, head) {
+    if (socket && socket._readableState && socket._readableState.pipes &&
+        socket._readableState.pipes._token) {
+      delete tunnelMap[socket._readableState.pipes._token]
+      logger.info("Tunnel close", socket._readableState.pipes._token)
+    }
+  });
+
+  proxy.on('error', function (err, req, res) {
+    logger.error('connection error', err)
+  });
+
+  proxyServer.listen(config.proxyPort);
+
+  // tunnel without security
+  var tunnel_server = new wstun.server_reverse();
+  tunnel_server.start(config.tunnelServerPort)
+}
