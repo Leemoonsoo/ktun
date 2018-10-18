a212bcc9effe7a0eaa044cd9d352d6943e6f67d3
Lee moon soo <leemoonsoo@gmail.com>
2018-10-17 21:02:29 -0700

    patch wstun data loss

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ if (program.port) {
     })
   })
 } else if (program.server && program.tunnel && program.proxy) {
+  patch_wstun()
   const client = new wstun.client_reverse;
   client.start(program.tunnel, program.server + "/_ktuncreate", program.proxy);
   logger.info("Starting tunnel", program.proxy)
@@ -92,8 +93,10 @@ function createServer(config) {
           logger.trace('Connection error', err)
         } else {
           // retry on error
-          logger.warn("Retry proxy request to tunnel", token, req.url)
-          proxy.web(req, res, { target: 'http://localhost:' + tunnelMap[token].port })
+          logger.warn("Proxy request error", token, req.url)
+
+          // Retry proxy request? disable for now.
+          // proxy.web(req, res, { target: 'http://localhost:' + tunnelMap[token].port })
         }
       });
     } else {
@@ -203,4 +206,114 @@ function createServer(config) {
   // tunnel without security
   var tunnel_server = new wstun.server_reverse();
   tunnel_server.start(config.tunnelServerPort)
+}
+
+
+/**
+ * Copy the source code here of https://github.com/MDSLab/wstun/blob/master/lib/client_reverse.js
+ * with patch https://github.com/MDSLab/wstun/pull/15 applied.
+ * Until new version is released with patch applied.
+ */
+function patch_wstun() {
+  var WebSocketClient = require('websocket').client;
+  var net = require("net");
+
+  var bindSockets = require('@mdslab/wstun/lib/bindSockets_reverse')
+
+  wst_client_reverse = function() {
+    this.wsClientForControll = new WebSocketClient();
+  };
+
+  wst_client_reverse.prototype.start = function(portTunnel, wsHostUrl, remoteAddr) {
+
+    //Getting paramiters
+    var url = require("url");
+    var urlWsHostObj = url.parse(wsHostUrl);
+    var _ref1 = remoteAddr.split(":"), remoteHost = _ref1[0], remotePort = _ref1[1];
+
+    var proto = wsHostUrl.split(":")[0];
+    if(proto == "wss")
+      require("../lib/https_override");
+
+    url = "" + wsHostUrl + "/?dst=" + urlWsHostObj.hostname+":"+portTunnel;
+
+    logger.info("[SYSTEM] - Connecting to", wsHostUrl);
+    logger.info("[SYSTEM] --> exposing", remoteAddr, "on port", portTunnel);
+
+    //Connection to Controll WS Server
+    this.wsClientForControll.connect(url, 'tunnel-protocol');
+
+    this.wsClientForControll.on('connect', (function(_this){
+
+      return function(wsConnectionForControll) {
+
+        logger.info("[SYSTEM] --> TCP connection established!");
+
+        wsConnectionForControll.on('message', function(message) {
+
+          //Only utf8 message used in Controll WS Socket
+          var parsing = message.utf8Data.split(":");
+
+          //Managing new TCP connection on WS Server
+          if (parsing[0] === 'NC'){
+
+            //Identification of ID connection
+            var idConnection = parsing[1];
+
+            this.wsClientData = new WebSocketClient();
+            this.wsClientData.connect(wsHostUrl+"/?id="+idConnection, 'tunnel-protocol');
+
+            //Management of new WS Client for every TCP connection on WS Server
+            this.wsClientData.on('connect', (function(_this){
+
+              return function(wsConnectionForData){
+
+                //Waiting of WS Socket with WS Server
+                wsConnectionForData.socket.pause();
+
+                //DEBUG logger.info("Connected wsClientData to WS-Server for id "+parsing[1]+" on localport::"+wsConnectionForData.socket.localPort);
+                logger.info("[SYSTEM] --> Start TCP connection on client to "+remoteHost+":"+remotePort);
+
+                tcpConnection(wsConnectionForData, remoteHost, remotePort);
+
+              }
+            })(this));
+
+          }
+        });
+
+      }
+
+    })(this));
+
+
+    //Management of WS Connection failed
+    this.wsClientForControll.on('connectFailed', function(error) {
+      logger.info("[SYSTEM] --> WS connect error: " + error.toString());
+    });
+
+
+  };
+
+  function tcpConnection(wsConn, host, port){
+
+    var tcpConn = net.connect( {port: port, host: host}, function(){});
+    bindSockets(wsConn, tcpConn);
+
+    tcpConn.on("connect",function(){
+      //Resume of the WS Socket after the connection to WS Server
+      wsConn.socket.resume();
+    });
+
+    tcpConn.on('error',(function(_this){
+      return function(request){
+        logger.info("[SYSTEM] --> "+request);
+      }
+    })(this));
+
+    //wst_client_reverse
+
+  }
+
+  wstun.client_reverse = wst_client_reverse
 }
